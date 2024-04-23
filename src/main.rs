86f8e5c581cde8fbcdac3b310bc8063ced2bf845
Bora <>
2024-04-23 11:17:29 +0300

    I think it's done

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod util;
+mod mandelbrot;
 use image::{Rgb, RgbImage};
 use show_image::create_window;
 
-use crate::util::to_showable_image;
+use crate::{mandelbrot::ColorMap, util::to_showable_image};
 use text_io::read;
 
 enum Color {
@@ -36,9 +37,12 @@ fn get_board_color(x: u32, y: u32, board_width: u32) -> Rgb<u8> {
 }
 
 fn chessboard_exercice() -> RgbImage {
-    println!("enter the board size");
-    let cell_count : u32 = read!();
-    let mut image  = image::RgbImage::new(cell_count, cell_count);
+    // Prompt the user to enter the size of the board
+    println!("Enter the board size:");
+    let cell_count: u32 = read!();
+
+    // Create a new RgbImage with the given board size
+    let mut image = RgbImage::new(cell_count, cell_count);
     for x in 0..cell_count {
         for y in 0..cell_count {
             image.put_pixel(x, y, get_board_color(x,y, cell_count));
@@ -64,7 +68,7 @@ mod tests {
     }
     #[test]
     fn test_get_board_color_40by40() {
-        // Test for a 40x40 chessboard with specific coordinates
+        // Test for a 40x40 chessboard with some coordinates
         assert_eq!(get_board_color(0, 0, 40), Color::WHITE.pixel());
         assert_eq!(get_board_color(5, 5, 40), Color::WHITE.pixel());
         assert_eq!(get_board_color(39, 0, 40), Color::BLACK.pixel());
@@ -76,37 +80,17 @@ mod tests {
 }
 
 
-
-
-
-fn mandelbrot_exercice() -> anyhow::Result<RgbImage> {
-    let width = 1024;
-    let height = 1024;
-    let mut image = RgbImage::new(width, height);
-
-    println!("enter the space coordinates");
-    let input_string: String = read!("{}\n");
+fn mandelbrot_plot(
+                    width: u32, 
+                    height: u32, 
+                    mandelbrot_x_min: f64, 
+                    mandelbrot_x_max: f64, 
+                    mandelbrot_y_min: f64,
+                    mandelbrot_y_max: f64,
+                    cmap: Box<dyn ColorMap> ) -> RgbImage {
     
-    let parts: Vec<&str> = input_string.split(';').collect();
-    // Extract individual parts into variables, setting defaults if parsing fails
-    let mandelbrot_x_min: f64 = match parts.get(0) {
-        Some(s) => s.parse().unwrap_or(-2.0),
-        None => -2.0,
-    };
-    let mandelbrot_x_max: f64 = match parts.get(1) {
-        Some(s) => s.parse().unwrap_or(2.0),
-        None => 2.0,
-    };
-    let mandelbrot_y_min: f64 = match parts.get(2) {
-        Some(s) => s.parse().unwrap_or(-1.5),
-        None => -1.5,
-    };
-    let mandelbrot_y_max: f64 = match parts.get(3) {
-        Some(s) => s.parse().unwrap_or(1.5),
-        None => 1.5,
-    };
-
-    let max_iteration = 100;
+    let mut image = RgbImage::new(width, height);
+    let max_iteration = 100;  
 
     for px in 0..width {
         for py in 0..height {
@@ -125,18 +109,67 @@ fn mandelbrot_exercice() -> anyhow::Result<RgbImage> {
                 iteration += 1;
             }
 
-            // Color pixel based on the number of iterations
-            let color = if iteration == max_iteration {
-                Rgb([0, 0, 0]) // Black for points inside the set
-            } else {
-                // Adjust color based on iteration
-                let brightness = (iteration as f64 / max_iteration as f64) * 255.0;
-                Rgb([brightness as u8, brightness as u8, brightness as u8])
-            };
-
-            image.put_pixel(px, py, color);
+            image.put_pixel(px, py, cmap.color(iteration));
         }
     }
+    return image;
+
+}
+
+
+fn mandelbrot_exercice() -> anyhow::Result<RgbImage> {
+    let width = 1024;
+    let height = 1024;
+
+    // Prompt the user to enter the space coordinates
+    println!("enter the space coordinates");
+    let input_string: String = read!("{}\n");
+    
+    // Split the input string into individual parts
+    let parts: Vec<&str> = input_string.split(';').collect();
+    
+    // Extract individual parts into variables, setting defaults if parsing fails
+    let mandelbrot_x_min: f64 = match parts.get(0) {
+        Some(s) => s.parse().unwrap_or(-2.0),
+        None => -2.0,
+    };
+    let mandelbrot_x_max: f64 = match parts.get(1) {
+        Some(s) => s.parse().unwrap_or(2.0),
+        None => 2.0,
+    };
+    let mandelbrot_y_min: f64 = match parts.get(2) {
+        Some(s) => s.parse().unwrap_or(-1.5),
+        None => -1.5,
+    };
+    let mandelbrot_y_max: f64 = match parts.get(3) {
+        Some(s) => s.parse().unwrap_or(1.5),
+        None => 1.5,
+    };
+
+    println!("Drawing on set x [{},{}] and y [{},{}]", mandelbrot_x_min, mandelbrot_x_max, mandelbrot_y_min, mandelbrot_y_max);
+    
+    // Prompt the user to enter the colormap
+    println!("enter the colormap ['c' or 'gs'] default: gs");
+    let input_string: String = read!("{}\n");
+
+    // Select the appropriate colormap based on user input
+    let maper: Box<dyn ColorMap> = match input_string.as_str() {
+        "c" => Box::new(mandelbrot::ColoredColorMap {
+            max_iterations: 100, // Maximum iterations for the Mandelbrot calculation
+            colorgrad: colorgrad::turbo(), // Color gradient for colored colormap
+        }),
+        _ => Box::new(mandelbrot::GrayMap {
+            max_iterations: 100, // Maximum iterations for the Mandelbrot calculation
+        }),
+    };
+
+    // Generate the Mandelbrot image
+    let image = mandelbrot_plot(
+        width, height, 
+        mandelbrot_x_min, mandelbrot_x_max, mandelbrot_y_min, mandelbrot_y_max,
+        maper
+    );
+    
     return Ok(image);
 }
 
--- src/mandelbrot.rs
+++ src/mandelbrot.rs
@@ -1,8 +1,52 @@
-use image::RgbImage;
+use colorgrad::Gradient;
+use image::Rgb;
 
 // ColorMap trait allow to define a color map for the mandelbrot set
 // The color method should return the color of the pixel for an iteration "i" between 0 and max_iterations.
 pub trait ColorMap {
     fn color(&self, i: u32) -> image::Rgb<u8>;
     fn get_max_iterations(&self) -> u32;
+}
+
+
+pub struct ColoredColorMap {
+    pub(crate) max_iterations: u32,
+    pub(crate) colorgrad: Gradient
+} 
+
+impl ColorMap for ColoredColorMap {
+    fn color(&self, i: u32) -> image::Rgb<u8> {
+        if i == self.max_iterations {
+            return Rgb([0, 0, 0]); // Black for points inside the set
+        }
+        //  Calculate the color using the gradient based on the iteration count
+        let rgba_color = self.colorgrad.at(i as f64 / self.max_iterations as f64).to_rgba8();
+        image::Rgb([rgba_color[0], rgba_color[1], rgba_color[2]])
+    }
+
+    fn get_max_iterations(&self) -> u32 {
+        self.max_iterations
+    }
+}
+
+pub struct GrayMap {
+    pub(crate) max_iterations: u32,
+}
+
+impl ColorMap for GrayMap {
+    fn color(&self, i: u32) -> image::Rgb<u8> {
+        let color = if i == self.max_iterations {
+            Rgb([0, 0, 0]) // Black for points inside the set
+        } else {
+            // Adjust color based on iteration
+            let brightness = (i as f64 / self.max_iterations as f64) * 255.0;
+            Rgb([brightness as u8, brightness as u8, brightness as u8])
+        };
+        
+        return color;
+    }
+
+    fn get_max_iterations(&self) -> u32 {
+        self.max_iterations
+    }
 }
